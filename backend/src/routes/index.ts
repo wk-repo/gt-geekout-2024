fca79517f463b17d2bf111a8ceda7ed678f33af2
wongkin_li <wongkin_li@estl.sg>
2024-06-06 12:41:51 +0800

    Clean up backend

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,23 +1,23 @@
-import { Router } from "express";
+import { Router } from 'express'
 import {
-    createTodo,
-    getAllTodos,
-    getTodosByDate,
-    deleteTodoById,
-    getTodoById,
-    updateTodoById,
-    createRandomTodo,
-    generateTodoContent
-} from "./methods";
+  createTodo,
+  getAllTodos,
+  getTodosByDate,
+  deleteTodoById,
+  getTodoById,
+  updateTodoById,
+  createRandomTodo,
+  //   generateTodoContent,
+} from './methods'
 
-const forwardRouter = Router();
-forwardRouter.post("/todos", createTodo);
-forwardRouter.post("/todos/:id/generate", generateTodoContent);
-forwardRouter.get("/todos", getAllTodos);
-forwardRouter.get("/todos", getTodosByDate);
-forwardRouter.get("/todos/:id", getTodoById);
-forwardRouter.put("/todos/:id", updateTodoById);
-forwardRouter.delete("/todos/:id", deleteTodoById);
-forwardRouter.post("/todos/random", createRandomTodo);
+const forwardRouter = Router()
+forwardRouter.post('/todos', createTodo)
+// forwardRouter.post("/todos/:id/generate", generateTodoContent);
+forwardRouter.get('/todos', getAllTodos)
+forwardRouter.get('/todos', getTodosByDate)
+forwardRouter.get('/todos/:id', getTodoById)
+forwardRouter.put('/todos/:id', updateTodoById)
+forwardRouter.delete('/todos/:id', deleteTodoById)
+forwardRouter.post('/todos/random', createRandomTodo)
 
-export default forwardRouter;
+export default forwardRouter

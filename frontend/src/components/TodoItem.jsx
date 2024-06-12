fb698fcfa81820be6944f6c8dae80ab66159402f
ddfung <danielfung.yh@gmail.com>
2024-06-12 17:17:02 +0800

    checkpoint-2 init

--- frontend/src/components/TodoItem.jsx
+++ frontend/src/components/TodoItem.jsx
@@ -4,10 +4,6 @@ import CONFIG from '../config'
 import crossIcon from '../icons/cross.svg'
 
 function TodoItem(props) {
-  //* Exercise 3B
-  const [completed, setCompleted] = useState(props.completed)
-
-  //* updateTodoItem function is used to update the state of completion of the TodoItem by making an api call via the PUT method
   const updateTodoItem = async (newCompleted) => {
     try {
       await axios.put(`${CONFIG.API_ENDPOINT}/todos/${props.id}`, {
@@ -20,31 +16,15 @@ function TodoItem(props) {
     }
   }
 
+  //* Exercise 2B
   const deleteTodoItem = async () => {
-    try {
-      await axios.delete(`${CONFIG.API_ENDPOINT}/todos/${props.id}`)
-      props.refreshToDos() // Call refreshToDos after the todo item is successfully deleted
-    } catch (error) {
-      console.error('Error deleting todo item:', error)
-    }
-  }
-
-  //* toggleCompleted function updates the TodoItem 'completed' state based on the whether he checkbox is checked or not
-  const toggleCompleted = async () => {
-    //* Insert code logic here
+    //* Fill in code
   }
 
   return (
     <>
       <div className="todo-item">
-        <input
-          type="checkbox"
-          style={{ transform: 'scale(1.5)' }}
-
-          //* Add the following 2 properties to manage the change in state of the checkbox based on user inputs
-          // checked={add function here}
-          // onChange={add function here}
-        />
+        <input type="checkbox" style={{ transform: 'scale(1.5)' }} />
         <span style={{ flexGrow: 1, color: '#344054' }}>
           {props.description}
         </span>
@@ -53,7 +33,8 @@ function TodoItem(props) {
             backgroundColor: 'transparent',
             border: 'none',
           }}
-          onClick={() => deleteTodoItem()}
+          //* Remember to use the onClick property to trigger the deletion
+          // onClick={...}
         >
           <img src={crossIcon} alt="Delete" className="delete-icon" />
         </button>
